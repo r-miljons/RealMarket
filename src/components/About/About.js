03331b13916afe110189ff5fa715567271537939
Renars Miljons <reku.miljons@gmail.com>
2022-12-06 09:45:59 +0200

    about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,38 @@
-import React from 'react'
-import "./About.scss"
+import React from "react";
+import "./About.scss";
+import selectionImg from "../../assets/selection.svg";
+import shippingImg from "../../assets/shipping.svg";
+import tradeImg from "../../assets/trade.svg";
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+	return (
+		<section id="about">
+			<div className="content-container">
+				<div className="section">
+					<img src={selectionImg} alt="woman selecting a product" />
+					<h3>Expansive Catalog</h3>
+					<p>
+						Find what you're looking with ease or explore countless product
+						categories.
+					</p>
+				</div>
+				<div className="section">
+					<img src={tradeImg} alt="two men making a deal" />
+					<h3>Become a Seller</h3>
+					<p>
+						Start selling your products within a few minutes of signing up,
+						without fees!
+					</p>
+				</div>
+				<div className="section">
+					<img src={shippingImg} alt="woman carrying a package" />
+					<h3>Enterprise Solutions</h3>
+					<p>
+						A platform that you can rely on for you business, with great
+						support.
+					</p>
+				</div>
+			</div>
+		</section>
+	);
 }
